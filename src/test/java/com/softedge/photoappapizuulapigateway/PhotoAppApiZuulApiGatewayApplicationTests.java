9e9a88faf8ea9fdbbb27429c3ddddf1e848c0143
Nitesh Kumar N <niteshkumarn55@gmail.com>
2020-02-26 18:15:42 +0530

    Zuul Api Gateway - First commit

---
+++ src/test/java/com/softedge/photoappapizuulapigateway/PhotoAppApiZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softedge.photoappapizuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
